dcf16db8d0227ce7a4b61631a49c7696c3406a1d
luanribeirou <luanribeiro.aluno@unipampa.edu.br>
2022-03-16 23:08:22 -0300

    Erros

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+package POO_Final;
+
+
+import POO_Final.Digraph;
+import POO_Final.Graph;
+
 class Main {
   public static void main(String[] args) {
     Graph<String,String> g;
--- Queue.java
+++ Queue.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package POO_Final;
 
-/**
- *
- * @author Luan
- */
-public interface Queue {
-    
+public interface Queue extends Container{
+  Object getHead();
+  void enqueue(Object object);
+  Object dequeue();
 }
