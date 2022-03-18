8d3bed220aaef0b6275bce96563d6f9d38451388
pedroanto <95453438+pedroanto@users.noreply.github.com>
2022-03-18 15:10:34 -0300

    Create Comparable.java

---
+++ Comparable.java
@@ -0,0 +1,7 @@
+public interface Comparable{
+  boolean isLT(Comparable object);
+  boolean isLE(Comparable object);
+  boolean isGT(Comparable object);
+  boolean isEQ(Comparable object);
+  int compare(Comparable object);
+}
