b5c43b37e7082a41fa975f3ea5d4d974d29d2306
pedroanto <95453438+pedroanto@users.noreply.github.com>
2022-03-18 15:11:23 -0300

    Create Association.java

---
+++ Association.java
@@ -0,0 +1,27 @@
+public class Association extends AbstractObject{
+  protected Comparable key;
+  protected Object value;
+  public Association(Comparable key, Object value){
+    this.key = key;
+    this.value = value;
+  }
+  public Association(Comparable key){
+    this(key,null);
+  }
+  public Comparable getKey(){
+    return key;
+  }
+  public Object getValue(){
+    return value;
+  }
+  protected int compareTo(Comparable object){
+    Association association = (Association) object;
+    return key.compare(association.getKey());
+  }
+  public String toString(){
+    String result ="Association{"+key;
+    if(value != null)
+      result +=","value;
+    return +"}";
+  }
+}
