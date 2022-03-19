f96707e92ea871f723271585540c04bb68248ab6
pedroanto <95453438+pedroanto@users.noreply.github.com>
2022-03-19 16:45:12 -0300

    Create AbstractContainer.java

---
+++ AbstractContainer.java
@@ -0,0 +1,13 @@
+public abstract class AbstractContainer extends AbstractObject implements Container{
+  protected int count;
+  public int count;
+  public int getCount(){
+    return count;
+  }
+  public boolean isEmpty(){
+    return getCount()==0;
+  }
+  public boolean isFull(){
+    return false;
+  }
+}
