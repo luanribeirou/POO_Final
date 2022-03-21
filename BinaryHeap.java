b0be5c0685907c825f37b579e50ec8d659a7cf01
luanribeirou <luanribeiro.aluno@unipampa.edu.br>
2022-03-21 19:39:33 -0300

    Update BinaryHeap.java

--- BinaryHeap.java
+++ BinaryHeap.java
@@ -1,18 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package POO_Final;
-
-import java.util.PriorityQueue;
-
-/**
- *
- * @author Luan
- */
-public class BinaryHeap extends PriorityQueue implements Container{
-
-    public BinaryHeap(int i, Object vertex) {
+public class BinaryHeap extends Conteiner implements PriorityQueue{
+  protected Comparable [] array;
+  public BinaryHeap(int lenght){
+    array = Comparable[lenght+1];
+  }
+  public void purge(){
+    while(count>0)
+      array[count--] = null;
+  }
+  public void enqueue(Comparable object){
+    if(count == array.lenght-1)
+      throw new ContainerFullException();
+    ++count;
+    int i = count;
+    while(i > 1 && array [i/2].isGT(object)){
+      array[i] = array [i/2];
+      i /= 2;
     }
-    
+    array [i] = object;
+  }
+  public Comparable finMin(){
+    if(count == 0)
+      throw new ContainerEmptyException();
+    return array [1];
+  }
+  public Comparable dequeueMin(){
+    if(count == 0)
+      throw new ContainerEmptyException();
+  Comparable result = array[1];
+  Comparable last = array[count];
+    --count;
+    int i = 1;
+    while(2 * i < count+1){
+      int child = 2 * i;
+      if(child + 1 < count + 1 && array[child+1].isLT(array[child]))
+        child += 1;
+      if(last.isLE(array[child]))
+        break;
+      array[i] = array[child];
+      i = child;
+    }
+  array[i] = last;
+    return result;
+    }
 }
