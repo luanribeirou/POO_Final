fa73c362d5b4be870785c992b1d457067fcac01d
luanribeirou <luanribeiro.aluno@unipampa.edu.br>
2022-03-21 20:48:59 -0300

    Update BinaryHeap.java

--- BinaryHeap.java
+++ BinaryHeap.java
@@ -1,22 +1,15 @@
-import java.util.Enumeration;
-
-
-public class BinaryHeap extends AbstractContainer implements PriorityQueue{
+public class BinaryHeap extends Conteiner implements PriorityQueue{
   protected Comparable [] array;
   public BinaryHeap(int lenght){
-    array = new Comparable[lenght+1];
-  } 
+    array = Comparable[lenght+1];
+  }
   public void purge(){
     while(count>0)
       array[count--] = null;
   }
   public void enqueue(Comparable object){
-    if(count == array.length -1)
-      try {
-        throw new ContainerFullException();
-      } catch (ContainerFullException e) {
-        e.printStackTrace();
-      }
+    if(count == array.lenght-1)
+      throw new ContainerFullException();
     ++count;
     int i = count;
     while(i > 1 && array [i/2].isGT(object)){
@@ -25,19 +18,14 @@ public class BinaryHeap extends AbstractContainer implements PriorityQueue{
     }
     array [i] = object;
   }
-  public Comparable finMin() throws ContainerEmptyException{
+  public Comparable finMin(){
     if(count == 0)
       throw new ContainerEmptyException();
     return array [1];
   }
   public Comparable dequeueMin(){
     if(count == 0)
-      try {
-        throw new ContainerEmptyException();
-      } catch (ContainerEmptyException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-      }
+      throw new ContainerEmptyException();
   Comparable result = array[1];
   Comparable last = array[count];
     --count;
@@ -54,24 +42,4 @@ public class BinaryHeap extends AbstractContainer implements PriorityQueue{
   array[i] = last;
     return result;
     }
-  @Override
-  public void accept(Visitor visitor) {
-    // TODO Auto-generated method stub
-    
-  }
-  @Override
-  public Enumeration getEnumeration() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-  @Override
-  public Comparable findMin() {
-    // TODO Auto-generated method stub
-    return null;
-  }
-  @Override
-  protected int compareTo(Comparable arg) {
-    // TODO Auto-generated method stub
-    return 0;
-  }
 }
