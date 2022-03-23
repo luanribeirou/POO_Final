8b007e41d74e0cd2adcd69ab56278102eeac7b57
pedroanto <95453438+pedroanto@users.noreply.github.com>
2022-03-22 22:31:33 -0300

    Update AbstractTree.java

--- AbstractTree.java
+++ AbstractTree.java
@@ -14,5 +14,22 @@ public abstract class AbstractTree
 	    visitor.postVisit (getKey ());
 	}
     }
-    // ...
+    
+    public void breadthFirstTraversal (Visitor visitor)
+    {
+	Queue queue = new QueueAsLinkedList ();
+	if (!isEmpty ())
+	    queue.enqueue (this);
+	while (!queue.isEmpty () && !visitor.isDone ())
+	{
+	    Tree head = (Tree) queue.dequeue ();
+	    visitor.visit (head.getKey ());
+	    for (int i = 0; i < head.getDegree (); ++i)
+	    {
+		Tree child = head.getSubtree (i);
+		if (!child.isEmpty ())
+		    queue.enqueue (child);
+	    }
+	}
+    }
 }
